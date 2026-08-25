a45b32615991597071a56addb52927b272d79570
jonas <jonas.humenu@gmail.com>
2026-08-25 12:24:43 +0100

    Fix: import not found failed

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -1,10 +1,8 @@
 import AboutMeSection from "../Components/AboutMeSection";
 import FAQSection from "../Components/FAQSection";
-import Footer from "../Components/Footer";
 import Hero from "../Components/Hero";
 import MessageSection from "../Components/MessageSection";
 import MomentSection from "../Components/MomentSection";
-import Navbar from "../Components/navbar";
 import ScheduleLesson from "../Components/ScheduleLesson";
 import TestimonialSection from "../Components/TestimonialSection";
 import TutorialSection from "../Components/TutorialSection";
@@ -15,8 +13,6 @@ export default function Home() {
 
   return (
     <div className="mt-12">
-      <Navbar />
-
       <Hero />
       <VideoSection />
       <AboutMeSection />
